80379212971f2aa4a4758ece451cfbaaac161109
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2021-12-27 00:44:35 +0200

    add 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   let promiseValue = { position, delay };
@@ -8,4 +10,30 @@ function createPromise(position, delay) {
     }
     reject(promiseValue);
   });
+}
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(elm) {
+  elm.preventDefault();
+
+  let delay = Number(elm.currentTarget.delay.value);
+  let step = Number(elm.currentTarget.step.value);
+  let amount = Number(elm.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
+  }
 }
